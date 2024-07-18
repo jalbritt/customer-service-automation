d10c71fd8bffeb5147c7fb414d2c2a81fba04eed
James Albritton <jalbritt@users.noreply.github.com>
2024-07-18 12:47:43 +0200

    todo

--- app/utils/database.py
+++ app/utils/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# TODO: Access the environment variable from configmap
 SQLALCHEMY_DATABASE_URL = "postgresql://myuser:postgrespass@host.docker.internal:5432/mydatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
